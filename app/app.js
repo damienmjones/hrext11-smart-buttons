f86273afa0793b09b26a192d1e846972f355455e
Damien Jones <damienjones@gmail.com>
2019-08-28 17:59:05 -0700

    Adding demo selector.

--- app/app.js
+++ app/app.js
@@ -1,42 +1,46 @@
-var ifttt_id = "gJZzNn_HHmO3cogsCTnBy_"
+/* sample data - remove for production */
+const demo_user = "damien"
+var demo_id = "gJZzNn_HHmO3cogsCTnBy_"
+const demo_url = "http://192.168.1.20:8080/jsfs.html"
+const demo_list = '<select id="button_demo_list"><option value="">or choose one from the list</option><option value="hrext11_orange">orange</option><option value="hrext11_dim_white" >dim/yellow</option><option value="hrext11_bright_white" >bright white</option><option value="hrext11_pink" >pink</option><option value="hrext11_red" >red</option><option value="hrext11_green_fade" >green fade</option><option value="hrext11_blue_blink" >blue blinking</option><option value="hrext11_disco" >disco</option></select>'
 
 $(function() {
-
   displayData();
 
-
-
-  $('.create').click(createAutomation)
-
-  $('#ifttt').change(function() {
-    console.log('ifttt_id')
-    create('ifttt_id',  $(this).val() );
-    console.log(get('ifttt_id'))
+  $('#button_create').click(function() {
+    createButton($(this).val());
   })
 
+  $('#button_demo_list').click(function() {
+    createButton($(this).val(), $(this).children('option:selected').text());$(this).val('');
+  })
 
 }) // end of document.ready
 
-const createAutomation = function() {
-    var key = $('#key').val();
-    var value = $('#value').val();
-
-    if (get(key) !== null) {
-      alert('key already exists');
-    } else {
-      create(key, value);
-      displayData();
+const createButton = function(id, val) {
+    if (get(id) !== null) {
+      alert('trigger already exists');return;
     }
+    create(id, val||id);
+    displayData();
+    // then open the edit part
 }
 
 const displayData = function() {
   console.log('displayData')
+
   $('section#buttons').html('');
 
   for (var i = 0; i < count(); i++) {
-    $('section#buttons').append(
-    `<div id="${getKey(i)}"><button class="automate">${get(getKey(i))}</button> <a class="favorite" href="#">↥ favorite</a> <a class="delete" href="#">✘ delete</a> <a class="edit" href="#">✎ edit</a></div>`
-    )
+    let key = getKey(i), val=get(key);
+    if (key == "ifttt_id") {
+      $('#ifttt').val(val);
+    } else {
+      $('section#buttons').append(
+        `<div id="${getKey(i)}"><button class="automate">${get(getKey(i))}</button> <a class="top" href="#">↥ top</a> <a class="delete" href="#">✘ delete</a> <a class="edit" href="#">✎ edit</a></div>`
+      );
+    }
+
   }
   bindButtonEvents();
 }
@@ -49,21 +53,25 @@ const bindButtonEvents = function() {
   $('a.delete').click(function() {
     deleteButton( $(this).parent() );
   })
+  $('a.top').click(function() {
+    topButton( $(this).parent() );
+  })
   $('button.automate').click(function() {
     automateButton( $(this).parent() );
   })
 }
 
-// pass parent div cause we're going to be working with the whole thing a lot
+const topButton = function($div) {
+  const id = $div.attr('id');
+  console.log('topButton(' + id +')');
+}
+
 const editButton = function($div) {
   const id = $div.attr('id');
   console.log('editButton(' + id +')');
-
-  // var key = $('#key').val();
-  // var value = $('#value').val();
 }
 
-// pass parent div cause we're going to be working with the whole thing a lot
+
 const deleteButton = function($div) {
   const id = $div.attr('id');
   console.log('deleteButton(' + id +')');
