ef597c400d7dbd03d7978edbeeb46e9cd91b5834
Damien Jones <damienjones@gmail.com>
2019-08-30 03:19:49 -0700

    Exising demo code.

--- app/demo.js
+++ app/demo.js
@@ -1,9 +1,22 @@
 const demo_user = "damien"
 var demo_id = "gJZzNn_HHmO3cogsCTnBy_"
 const demo_url = "http://192.168.1.20:8080/jsfs.html"
-const demo_list = '<br><select id="button_demo_list"><option value="">or choose one from the list (demo)</option><option value="hrext11_orange">orange</option><option value="hrext11_dim_white" >dim/yellow</option><option value="hrext11_bright_white" >bright white</option><option value="hrext11_pink" >pink</option><option value="hrext11_red" >red</option><option value="hrext11_green_fade" >green fade</option><option value="hrext11_blue_blink" >blue blinking</option><option value="hrext11_disco" >disco</option></select>'
+const demo_list = `
+<select id="button_demo_list">
+<option value="">or choose one from the list (demo)</option>
+<option value="hrext11_orange">orange</option>
+<option value="hrext11_dim_white" >dim/yellow</option>
+<option value="hrext11_bright_white" >bright white</option>
+<option value="hrext11_pink" >pink</option>
+<option value="hrext11_red" >red</option>
+<option value="hrext11_green_fade" >green fade</option>
+<option value="hrext11_blue_blink" >blue blinking</option>
+<option value="hrext11_disco" >disco</option>
+</select>`
 
 $(function() {
+
+  $('section#data_entry').append(demo_list);
   $('#button_demo_list').change(function() {
     new Button($(this).val(),$(this).children('option:selected').text()) ;
     $(this).val(''); // reset the select box
