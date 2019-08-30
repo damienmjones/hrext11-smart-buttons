324ff9d1f2f7de991fcd10c1ea0a52d50d86e967
Damien Jones <damienjones@gmail.com>
2019-08-30 00:17:13 -0700

    Getting a little better,  but editing (i.e. BASIC CRUD) still not fully working, and no styling of note.

--- app/app.js
+++ app/app.js
@@ -1,13 +1,9 @@
-
-
-
-
-
 $(function() {
-  displayData();
+  refreshButtons();
 
   $('#button_create').click(function() {
-    createButton($(this).val());
+    alert($('#button_trigger').val());
+    new Button($('#button_trigger').val());
   })
 
 
@@ -15,28 +11,23 @@ $(function() {
 }) // end of document.ready
 
 const createButton = function(id, val) {
+  alert(id);
     if (get(id) !== null) {
       alert('trigger already exists');return;
     }
     create(id, val||id);
-    displayData();
+    refreshButtons();
     // then open the edit part
 }
 
-const displayData = function() {
-  console.log('displayData')
-
+const refreshButtons = function() {   console.log('refreshButtons')
+  var buttonList = get('buttons') || [];
   $('section#buttons').html('');
-
-  for (var i = 0; i < count(); i++) {
-    let key = getKey(i), val=get(key);
-    if (key.indexOf("ifttt")) {
-      $('section#buttons').prepend(
-        `<div id="${getKey(i)}"><button class="automate">${get(getKey(i))}</button> <a class="top" href="#">↥ top</a> <a class="delete" href="#">✘ delete</a> <a class="edit" href="#">✎ edit</a></div>`
-      );
-    }
-
-  }
+  buttonList.forEach(function(button) {
+    $('section#buttons').prepend(
+      `<div id="${button.id}"><button class="automate ${button.style}">${button.name}</button> <a class="top" href="#">↥ top</a> <a class="delete" href="#">✘ delete</a> <a class="edit" href="#">✎ edit</a></div>`
+    );
+  })
   bindButtonEvents();
 }
 
@@ -45,8 +36,14 @@ const bindButtonEvents = function() {
   $('a.edit').click(function() {
     editButton( $(this).parent() );
   })
+  $('.button_name').change(function() {
+    setProp('button', $(this).parent().attr('id'), 'name', $(this).val())
+  })
+  $('.button_style').click(function() {
+    setProp('style', $(this).parent().attr('id'), 'name', $(this).val())
+  })
   $('a.delete').click(function() {
-    deleteButton( $(this).parent() );
+     deleteObj('buttons',$(this).parent().attr('id'));
   })
   $('a.top').click(function() {
     topButton( $(this).parent() );
@@ -61,7 +58,7 @@ const topButton = function($div) {
   console.log('topButton(' + id +')');
 }
 
-const colorpicker=`<select>
+const colorpicker=`<select class="button_style">
 <option class="white" value="white">Select a button color...</option>
 <option class="yellow" value="yellow">Yellow</option>
 <option class="orange" value="orange">Orange</option>
@@ -77,7 +74,7 @@ const editButton = function($div) {
   console.log('editButton(' + id +')');
 
   $div.append(
-    `<input class="name" value="button name">${colorpicker}`
+    `<input class="button_name" value="${getProp('buttons',id,'name')}">${colorpicker}`
   );
 }
 
@@ -85,7 +82,7 @@ const deleteButton = function($div) {
   const id = $div.attr('id');
   console.log('deleteButton(' + id +')');
   remove(id);
-  displayData();
+  refreshButtons();
 }
 
 const automateButton = function($div) {
@@ -98,5 +95,4 @@ const automateButton = function($div) {
 }
 
 
-
-// Look into: Access to XMLHttpRequest at 'https://maker.ifttt.com/trigger/hrext11_disco/with/key/gJZzNn_HHmO3cogsCTnBy_-UNg1rOYaqo579Lepel7b' from origin 'null' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource
+// FUTURE: Look into: Access to XMLHttpRequest at 'https://maker.ifttt.com/trigger/hrext11_disco/with/key/gJZzNn_HHmO3cogsCTnBy_-UNg1rOYaqo579Lepel7b' from origin 'null' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource
--- app/data.js
+++ app/data.js
@@ -1,21 +1,104 @@
+const buttons= [];
 const LS = window.localStorage;
 
-const getKey = function(index) {
-  return LS.key(index)
+const set = function(key, value) {
+  LS.setItem(key, JSON.stringify(value));
 }
-const create = function(key, value) {
-  LS.setItem(key, value);
-}
+
 const get = function(key) {
-  let keyValue = LS.getItem(key)
-  try {keyValue = JSON.parse(keyValue);} catch(err) {}
-  return keyValue;
+  let obj = LS.getItem(key)
+  try {obj = JSON.parse(obj);} catch(err) {}
+  return obj;
 }
 
-const remove = function(key) {
-  return LS.removeItem(key);
+const saveButton = function (id, name, style) {
+
+
 }
 
-const count = function(key) {
-  return LS.length;
+const saveObj = function(category, obj) {
+  const arr = get(category) || [];
+  arr.push(obj); // duplicates actually ok? this could be a problem
+  set(category, arr);
+  refreshObj(category)
 }
+
+const getObj = function(category,id) {
+  const arr = get(category) || [];
+  return arr.find(function(obj) {
+    return (id==obj["id"])
+  })
+}
+
+const deleteObj = function(category,id) {
+  const arr = get(category) || [];
+  set(category,arr.filter(function(obj){
+    return (id!=obj["id"])
+  }))
+  refreshObj(category);
+}
+
+const topObj = function(category,id) {
+  const arr = get(category) || [];
+  // do this thing
+  refreshObj(category);
+}
+
+const setProp = function(category, id, prop, val) {
+  console.log(category, id, prop, val);
+  getObj(category,id)[prop] = val;
+}
+
+const getProp = function(category, id, prop) {
+  return getObj(category,id)[prop];
+}
+
+const refreshObj = function(category) {
+    if(category==='buttons') { refreshButtons(); }
+}
+
+const updateButtonName = function(id, name) {
+  getObj(id)["name"]=name;
+  refreshButtons();
+}
+
+const updateButtonStyle = function(id, name) {
+  getObj(id)["style"]=style;
+  refreshButtons();
+
+}
+
+// BUTTON STUFF
+const Button = function (id, name, style) {
+  this.id = id;
+  this.name = name || id;
+  this.style = style || "";
+  this.save()
+};
+
+Button.prototype.setName = function(name) {
+  this.name = name || this.id;
+  this.save()
+}
+
+Button.prototype.setStyle = function(style) {
+  this.style = style || "";
+  this.save()
+}
+
+Button.prototype.delete = function(id) {
+  deleteObj('buttons',this);
+  this.save()
+}
+
+Button.prototype.HTML = function() {
+  return `<div id="${this.id}"><button class="automate ${this.style}">${this.name}</button> <a class="top" href="#">↥ top</a> <a class="delete" href="#">✘ delete</a> <a class="edit" href="#">✎ edit</a></div>`
+}
+
+Button.prototype.save = function() {
+  saveObj('buttons',this);
+}
+
+//var b1 = new Button('hrext11_off', "Everything off")
+//b1.update
+
